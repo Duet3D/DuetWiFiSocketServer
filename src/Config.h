f66233ce1bc2318629dbe3548c744f8bc3a8096b
David Crocker <dcrocker@eschertech.com>
2017-05-04 21:32:54 +0100

    Changed version number to 1.19alpha1

--- src/Config.h
+++ src/Config.h
@@ -3,7 +3,7 @@
 #ifndef CONFIG_H_INCLUDED
 #define CONFIG_H_INCLUDED
 
-const char* const firmwareVersion = "1.10-alpha";
+const char* const firmwareVersion = "1.19-alpha1";
 //const char* const softApName = "DuetWiFi";
 
 //#define SPI_DEBUG
